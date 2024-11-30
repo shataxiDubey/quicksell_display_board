ed2d1ffdfa5bc11957728f4349d707e56bbb7ac3
Shataxi Dubey <dubey.shataxi3010@gmail.com>
2024-12-01 03:23:00 +0530

    Updates

---
+++ precache-manifest.ed5547c3f5441dc4c6f4bb542c22870c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046a016a006f913748fec83f85d5f5e0",
+    "url": "/quicksell_display_board/index.html"
+  },
+  {
+    "revision": "8186d226c95aa1fa0940",
+    "url": "/quicksell_display_board/static/css/main.b64a8762.chunk.css"
+  },
+  {
+    "revision": "0ab699fdf3df76516cf3",
+    "url": "/quicksell_display_board/static/js/2.acdaa6bc.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/quicksell_display_board/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "8186d226c95aa1fa0940",
+    "url": "/quicksell_display_board/static/js/main.034f95e4.chunk.js"
+  },
+  {
+    "revision": "722ebb800a513e5dc5e8",
+    "url": "/quicksell_display_board/static/js/runtime~main.62a9e7fb.js"
+  }
+]);
